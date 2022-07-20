d07ce464aa43f0e3d5792bdc1665c51161262d2f
Phillip Marques <phillip.silva@capgemini.com>
2022-07-20 18:39:18 -0300

    Relação entre tipos de variáveis

--- app.js
+++ app.js
@@ -2,13 +2,20 @@
 let button = document.getElementById('button');
 let input1 = document.getElementById('input1');
 let input2 = document.getElementById('input2');
-function somar(a, b) {
-    return a + b;
+function somarNumeros(a, b, printarResultado, frase) {
+    const result = a + b;
+    if (printarResultado) {
+        console.log(frase + result);
+    }
+    return result;
 }
+let printarResultado = true;
+let frase;
+frase = "O número é ";
 if (button) {
     button.addEventListener('click', () => {
         if (input1 && input2) {
-            console.log(somar(Number(input1.value), Number(input2.value)));
+            console.log(somarNumeros(Number(input1.value), Number(input2.value), printarResultado, frase));
         }
     });
 }
--- app.ts
+++ app.ts
@@ -2,14 +2,22 @@ let button = document.getElementById('button');
 let input1 = document.getElementById('input1') as HTMLInputElement;
 let input2 = document.getElementById('input2') as HTMLInputElement;
 
-function somar(a: number, b: number){
-    return a + b;
+function somarNumeros(a: number, b: number, printarResultado: boolean, frase: string){
+   const result = a + b;
+   if(printarResultado){
+    console.log(frase + result);
+   }
+   return result
 }
+
+let printarResultado = true;
+let frase: string;
+frase = "O número é "
 
 if(button){
     button.addEventListener('click', () =>{
         if(input1 && input2){
-            console.log(somar(Number(input1.value), Number(input2.value)));
+            console.log(somarNumeros(Number(input1.value), Number(input2.value), printarResultado, frase));
         }
     })
 }
